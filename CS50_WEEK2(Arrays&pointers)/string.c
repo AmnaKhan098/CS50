b6f1afec3fd94df53b3f7ce72870b959ed770721
amnakhan098 <amnakhang09876@gmail.com>
2026-04-17 23:38:02 +0500

    ADD: string file added

---
+++ CS50_WEEK2(Arrays&pointers)/string.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    
+    // print every charcter
+    // char movies[]={'b','a','t','m','a','n'};
+    // for(int i=0; i<7; i++){
+    //     printf("%c", movies[i]);
+
+    // }
+
+
+
+    // print every chracater
+    // char movies[]="batman";
+    // for(int i=0; i<6; i++){
+    //     printf("%c", movies[i]);
+    
+    // }
+   
+
+
+// print character of each string
+    // char movies[3][6]={"gfdth","wsxcd","qweds"};
+    // for(int i=0; i<3;i++){
+    //     printf("%c\n", movies[i][i]);
+    // }
+
+
+
+// prints every movie
+//    char movies[3][6]={"gfdth","wsxcd","qweds"};
+//     for(int i=0; i<3;i++){
+//         printf("%s\n", movies[i]);
+//     }
+
+
+
+    return 0;
+}
